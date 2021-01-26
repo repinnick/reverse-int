4a3027cf4af51751cfe0691d4fb91d49507bcd2f
igotbitches <check.point9917@yandex.ru>
2021-01-26 13:36:39 +0300

    feat: create function for reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) n = -n;
+
+    let array = new Array();
+    while (n != 0) {
+        array.push(n % 10);
+        n = (n - (n % 10)) / 10;
+    }
+    return array.join("");
+};
